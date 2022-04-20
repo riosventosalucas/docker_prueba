9c352dbac8b25626557eb8f5c5b0e54f64ed8db9
Lucas Rios Ventosa <lrios@provincianet.com.ar>
2022-04-20 13:23:04 -0300

    added powershell scripts

--- backend-test/build_and_create_container.ps1
+++ backend-test/build_and_create_container.ps1
@@ -3,12 +3,12 @@
 cls
 
 $VERSION="1.0.0"
-$APP_NAME="frontendapp"
+$APP_NAME="backendapp"
 $WORKDIR=$(pwd)
 $DOCKER_DAEMON='docker'
 $CURRENT_VERSION=0
 $NEW_VERSION=0
-$LOCAL_PORT=8000
+$LOCAL_PORT=8001
 $REMOTE_PORT=80
 
 Write-host "[ INFO ] SCRIPT VERSION: $VERSION`n"
@@ -25,7 +25,7 @@ if ( -not $ALREADY_EXIST) {
     Write-host "`t[ INFO ] Configurando version en 1."
     $CURRENT_VERSION=1
     $NEW_VERSION=$CURRENT_VERSION
-    Write-output "FROM nginx:alpine`nCOPY dist/ /usr/share/nginx/html" | Out-file -encoding utf8 $WORKDIR/Dockerfile
+    Write-output "FROM python:alpine`nCOPY dist/ /usr/src/`nWORKDIR /usr/src`nRUN python3 -m venv env`n RUN source env/bin/activate`nRUN pip3 install -r requirements.txt" | Out-file -encoding utf8 $WORKDIR/Dockerfile
 }
 else {
     $CURRENT_VERSION=$(docker image ls | grep $APP_NAME | ForEach-Object { $_.split(" ")[3] })
@@ -43,9 +43,9 @@ if ($OUTPUT -like "*naming*") {
     Write-host "[ OK ] Image creada correctamente..."
 
     Write-host "`n[ INFO ] Generando contenedor..."
-
     docker rm -f $APP_NAME
-    $CONTAINER_ID=$(docker run -d -p ${LOCAL_PORT}:${REMOTE_PORT} --name $APP_NAME ${APP_NAME}:${NEW_VERSION})
+    
+    $CONTAINER_ID=$(docker run -d -p ${LOCAL_PORT}:${REMOTE_PORT} --name $APP_NAME ${APP_NAME}:${NEW_VERSION} python3 /usr/src/app.py)
     Write-host "`t[ INFO ] ID Contenedor $CONTAINER_ID"
     Write-host "`t[ INFO ] Contenedor ejecutandose en: http://localhost:$LOCAL_PORT"
 }
